263f9d980f4ac948ca01518a43f98d3be794b6f9
Zhou Shenmin <shenminzhou@gmail.com>
2022-01-05 19:40:06 +0800

    Update waveform init logic

--- src/components/WaveForm.tsx
+++ src/components/WaveForm.tsx
@@ -1,6 +1,7 @@
-import { FC, useEffect, useState } from 'react'
+import { FC, useEffect, useState, useRef } from 'react'
 import { getSampling, doVideo, doVideoWithDefault, PIXELS_PER_SECOND } from '../utils'
 import { useSelector } from '../state'
+import cn from 'classnames'
 import styles from './WaveForm.module.less'
 
 interface Props {}
@@ -8,67 +9,17 @@ interface Props {}
 const WAVEFORM_ID = 'srt-player-waveform'
 
 export const WaveForm: FC<Props> = () => {
-  const file = useSelector(s => s.files.selected)
+  const file = useSelector(s => s.files.selected) as string
+  const [ready, setReady] = useState(false)
   const [offset, setOffset] = useState<number | undefined>(undefined)
   const [replayPos, setReplayPos] = useState<number>(-1)
-  const [images, setImages] = useState<string[]>([])
 
+  const images = useImages(file, setOffset, () => setReady(true))
   useEffect(() => {
-    let cb = () => {}
-    getSampling(file as string).then(blobs => {
-      if (!blobs) return
-      const urls = blobs.map(b => URL.createObjectURL(b))
-      setImages(urls)
-      setTimeout(() => {
-        updatePosition(setOffset, true)
-        // wait for 2 seconds to avoid conflict with seekListener
-      }, 2000)
-      cb = () => {
-        urls.forEach(url => {
-          URL.revokeObjectURL(url)
-        })
-      }
-    })
-    return () => {
-      cb()
+    if (ready) {
+      return listenToVideoEvents(setOffset)
     }
-  }, [])
-
-  useEffect(() => {
-    return doVideoWithDefault(
-      video => {
-        let enable = true
-        const move = () => {
-          if (!enable) return
-          updatePosition(setOffset)
-          requestAnimationFrame(move)
-        }
-        const playListener = () => {
-          enable = true
-          move()
-        }
-        const stopListener = () => {
-          enable = false
-        }
-        const seekListener = () => {
-          updatePosition(setOffset, true)
-        }
-        setTimeout(() => {
-          if (!video.paused && !video.ended) playListener()
-          // a little more than 2 seconds
-        }, 2300)
-        video.addEventListener('play', playListener)
-        video.addEventListener('pause', stopListener)
-        video.addEventListener('seeked', seekListener)
-        return () => {
-          video.removeEventListener('play', playListener)
-          video.removeEventListener('pause', stopListener)
-          video.removeEventListener('seeked', seekListener)
-        }
-      },
-      () => {},
-    )
-  }, [])
+  }, [ready])
 
   useEffect(() => {
     function keyListener(e: KeyboardEvent) {
@@ -99,19 +50,16 @@ export const WaveForm: FC<Props> = () => {
   }, [replayPos])
 
   return (
-    <div className={styles['waveform']}>
+    <div className={cn(styles['waveform'], { [styles['ready']]: ready })}>
       <div
         id={WAVEFORM_ID}
         className={styles['waveform-container']}
-        style={{ display: offset === undefined ? 'none' : undefined }}
         onClick={e => {
           const { left } = (e.target as HTMLDivElement).getBoundingClientRect()
           setReplayPos(e.clientX - left)
         }}
       >
-        {images.map(url => (
-          <img key={url} src={url} />
-        ))}
+        {images}
         <div
           className={styles['replay-indicator']}
           style={{ transform: `translate3d(${Math.floor(replayPos)}px,0,0)` }}
@@ -122,6 +70,86 @@ export const WaveForm: FC<Props> = () => {
         />
       </div>
     </div>
+  )
+}
+
+const useImages = (file: string, setOffset: { (x: number): void }, onReady: { (): void }) => {
+  const [images, setImages] = useState<string[]>([])
+  const [loaded, setLoaded] = useState(false)
+  const loadedCount = useRef(0)
+
+  useEffect(() => {
+    let cb = () => {}
+    getSampling(file).then(blobs => {
+      if (!blobs) return
+      const urls = blobs.map(b => URL.createObjectURL(b))
+      setImages(urls)
+      cb = () => {
+        urls.forEach(url => {
+          URL.revokeObjectURL(url)
+        })
+      }
+    })
+    return () => {
+      cb()
+    }
+  }, [])
+
+  useEffect(() => {
+    if (loaded) {
+      updatePosition(setOffset, true)
+      setTimeout(onReady)
+    }
+  }, [loaded])
+
+  return (
+    <>
+      {images.map(url => (
+        <img
+          key={url}
+          src={url}
+          onLoad={() => {
+            loadedCount.current++
+            if (loadedCount.current === images.length) {
+              setLoaded(true)
+            }
+          }}
+        />
+      ))}
+    </>
+  )
+}
+
+const listenToVideoEvents = (setOffset: (x: number) => void) => {
+  return doVideoWithDefault(
+    video => {
+      let enable = true
+      const move = () => {
+        if (!enable) return
+        updatePosition(setOffset)
+        requestAnimationFrame(move)
+      }
+      const playListener = () => {
+        enable = true
+        move()
+      }
+      const stopListener = () => {
+        enable = false
+      }
+      const seekListener = () => {
+        updatePosition(setOffset, true)
+      }
+      if (!video.paused && !video.ended) playListener()
+      video.addEventListener('play', playListener)
+      video.addEventListener('pause', stopListener)
+      video.addEventListener('seeked', seekListener)
+      return () => {
+        video.removeEventListener('play', playListener)
+        video.removeEventListener('pause', stopListener)
+        video.removeEventListener('seeked', seekListener)
+      }
+    },
+    () => {},
   )
 }
 
