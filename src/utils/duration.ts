a6fd70964c6ea5c9fa93fcfdea0744755182cb52
Shenmin Zhou <shenminzhou@gmail.com>
2022-07-07 18:25:02 +0800

    throw error when file is not supported

--- src/utils/duration.ts
+++ src/utils/duration.ts
@@ -2,10 +2,14 @@ export async function getMediaDuration(f: File) {
   const video = document.createElement('video')
   video.preload = 'metadata'
 
-  return new Promise<number>(resolve => {
+  return new Promise<number>((resolve, reject) => {
     video.onloadedmetadata = () => {
       URL.revokeObjectURL(video.src)
       resolve(video.duration)
+    }
+
+    video.onerror = () => {
+      reject(`Failed to get duration of ${f.name}.`)
     }
 
     video.src = URL.createObjectURL(f)
